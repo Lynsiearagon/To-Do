182ea992b7031fecf9fe194eaf3b9af57017d5ab
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-07-12 14:55:30 -0400

    Add simple styling to To Do card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { ToDo as ToDoModel } from './models/todos';
 import ToDo from './components/ToDo';
+import { Container, Row } from 'react-bootstrap';
 
 function App() {
   const [toDos, setToDos] = useState<ToDoModel[]>([]);
@@ -21,11 +22,13 @@ function App() {
   }, []);
 
   return (
-    <div>
-      {toDos.map(todo => (
-        <ToDo toDo={todo} key={todo._id} />
-      ))}
-    </div>
+    <Container>
+      <Row xs={1} md={2} xl={3} className="g-4">
+        {toDos.map(todo => (
+          <ToDo toDo={todo} key={todo._id} />
+        ))}
+      </Row>
+    </Container>
   );
 }
 
